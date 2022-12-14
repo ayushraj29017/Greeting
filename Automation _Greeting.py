066472469075e7084092083fa7a698f255be6c72
AYUSH RAJ <109357257+ayushraj29017@users.noreply.github.com>
2022-12-14 19:00:55 +0530

    Add files via upload

---
+++ Automation _Greeting.py
@@ -0,0 +1,17 @@
+# Correct me if I'm missing something
+import time
+# Morning: till 12:00,AfterNoon till 18:00,Evening till 20:00,else Night
+name=input("Enter your Name-> ")
+hour=time.strftime('%H')
+minute=time.strftime('%M')
+second=time.strftime('%S')
+print("The current time in your area is->",hour,':', minute,':',second)
+if(int(hour)<12):
+    print("Good Morning!",name)
+elif(int(hour)<18):
+    print("Good Afternoon!",name)
+elif(int(hour)<20):
+    print("Good Evening!",name)
+else:
+    print("Good Night!",name,"\nSir, I think its time for you to sleep")
+ 
